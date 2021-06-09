32d45fc7af647de19e556dcebfeff8a83ebfbd93
korkem <73012254+kkorkemm@users.noreply.github.com>
2021-06-09 20:04:15 +0600

    Добавьте файлы проекта.

---
+++ Training/Navigation.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Training
+{
+    /// <summary>
+    /// Класс для навигации между страницами
+    /// </summary>
+    class Navigation
+    {
+        public static Frame MainFrame { get; set; }
+        public static Frame SubFrame { get; set; }
+    }
+}
